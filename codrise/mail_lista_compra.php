7d51c9eae9d651c132254cbc49437e9f1042a375
Luis Augusto Claudio Ponce <megabyte1507@gmail.com>
2015-10-30 16:22:17 -0500

    6 commit

--- codrise/mail_lista_compra.php
+++ codrise/mail_lista_compra.php
@@ -5,13 +5,37 @@ $asunto       = "Lista de Compra";
 $cuerpo =' 
 <html> 
 <head> 
-<title>Lista de Compra</title> 
+<title>Lista de Compra</title>
+<style>
+.enlaceboton {    
+font-family: verdana, arial, sans-serif; 
+font-size: 12pt; 
+font-weight: bold; 
+padding: 10px; 
+background-color: #00AAFF; 
+color: #fff; 
+text-decoration: none; 
+} 
+.enlaceboton:link, 
+.enlaceboton:visited { 
+border-top: 1px solid #00AAFF; 
+border-bottom: 2px solid #00AAFF; 
+border-left: 1px solid #00AAFF; 
+border-right: 2px solid #00AAFF; 
+border-radius: 5px;
+} 
+
+
+
+</style>
+
 </head> 
 <body> 
 <h1>Lista de Compra Nueva</h1> 
 <p> 
 <b>El  Usuario '. $_GET[usuario].' acaba de generar una nueva lista de compra.
 </p> 
+<p><a href="http://192.168.1.27/reserva/pdf/lista-de-compras" target="_blank" class="enlaceboton">Descargar Lista</a></p>
 </body> 
 </html> 
 '; 
