024666d9640f1aac0ccda8794907ee97fe25b5be
Luis Augusto Claudio Ponce <megabyte1507@gmail.com>
2015-10-30 15:15:52 -0500

    5 commit

--- codrise/mail_lista_compra.php
+++ codrise/mail_lista_compra.php
@@ -1,5 +1,4 @@
 <?php 
-$tipo=$_GET['tipo'];
 $destinatario ="luis.claudio@codrise.com";
 $remitente    ="luis.claudio@overprimegroup.com";
 $asunto       = "Lista de Compra"; 
@@ -42,6 +41,6 @@ $headers .= "Cc: soporte@rockdrillgroup.com\r\n";
 
 mail($destinatario,$asunto,$cuerpo,$headers);
 
-header('Location: http://192.168.1.27/reserva/pages/reserva-kit?tipo=$tipo')
+header('Location: http://192.168.1.27/reserva/pages/reserva-kit?tipo='.$_GET[tipo])
 
 ?>
