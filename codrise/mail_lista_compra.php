c16cb41488ec3c166c9a7cd314c5aee16ec0cb68
Luis Augusto Claudio Ponce <megabyte1507@gmail.com>
2015-10-30 12:26:31 -0500

    second commit

---
+++ codrise/mail_lista_compra.php
@@ -0,0 +1,46 @@
+<?php 
+$destinatario ="luis.claudio@codrise.com";
+$remitente    ="luis.claudio@overprimegroup.com";
+$asunto       = "Lista de Compra"; 
+$cuerpo =' 
+<html> 
+<head> 
+<title>Lista de Compra</title> 
+</head> 
+<body> 
+<h1>Listad de Compra Nueva</h1> 
+<p> 
+<b>Se acaba de  Generar  una nueva lista de compra.
+</p> 
+</body> 
+</html> 
+'; 
+
+$headers .= 'From: '.$remitente."\r\n".
+'Reply-To: '.$remitente."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+
+//para el envío en formato HTML 
+$headers .= "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=UTF-8\r\n"; 
+
+//dirección del remitente 
+//$headers .= "From: Luis Claudio <luis.claudio@overprimegroup.com>\r\n"; 
+
+//dirección de respuesta, si queremos que sea distinta que la del remitente 
+//$headers .= "Reply-To: mariano@desarrolloweb.com\r\n"; 
+
+//ruta del mensaje desde origen a destino 
+//$headers .= "Return-path: holahola@desarrolloweb.com\r\n"; 
+
+//direcciones que recibión copia 
+$headers .= "Cc: soporte@rockdrillgroup.com\r\n"; 
+
+//direcciones que recibirón copia oculta 
+//$headers .= "Bcc: pepe@pepe.com,juan@juan.com\r\n"; 
+
+mail($destinatario,$asunto,$cuerpo,$headers);
+
+header('Location: http://192.168.1.27/reserva/home')
+
+?>
