1eaa3c0aaa7044f6b2a73ab65cae83b23e76cb86
claudito <megabyte1507@gmail.com>
2015-12-17 16:03:12 -0500

    fix all ok

--- rockdrill/mail_lista_compra.php
+++ rockdrill/mail_lista_compra.php
@@ -2,8 +2,77 @@
 
 if (isset($_GET['usuario']) and isset($_GET['tipo'])) 
 {
+  
+$destinatario ="luis.claudio@rockdrillgroup.com";
+$remitente    ="reservas@rockdrillgroup.com";
+$asunto       = "Lista de Compra"; 
+$cuerpo =' 
+<html> 
+<head> 
+<title>Lista de Compra</title>
+<style>
+.enlaceboton {    
+font-family: verdana, arial, sans-serif; 
+font-size: 12pt; 
+font-weight: bold; 
+padding: 10px; 
+background-color: #00AAFF; 
+color: #fff; 
+text-decoration: none; 
+} 
+.enlaceboton:link, 
+.enlaceboton:visited { 
+border-top: 1px solid #00AAFF; 
+border-bottom: 2px solid #00AAFF; 
+border-left: 1px solid #00AAFF; 
+border-right: 2px solid #00AAFF; 
+border-radius: 5px;
+} 
 
-	echo "Lleno";
+
+
+</style>
+
+</head> 
+<body> 
+<h1>Lista de Compra Nueva - Modulo de Reservas Rockdrill</h1> 
+
+<p> 
+<b>El  Usuario '. $_GET[usuario].' acaba de generar una nueva lista de compra.
+</p> 
+<p><a href="http://192.168.1.7/rockdrill/reserva/pdf/lista-de-compras" target="_blank" class="enlaceboton">Descargar Lista</a></p>
+</body> 
+</html> 
+'; 
+
+$headers .= 'From: '.$remitente."\r\n".
+'Reply-To: '.$remitente."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+
+//para el envío en formato HTML 
+$headers .= "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=UTF-8\r\n"; 
+
+//dirección del remitente 
+//$headers .= "From: Luis Claudio <luis.claudio@overprimegroup.com>\r\n"; 
+
+//dirección de respuesta, si queremos que sea distinta que la del remitente 
+//$headers .= "Reply-To: mariano@desarrolloweb.com\r\n"; 
+
+//ruta del mensaje desde origen a destino 
+//$headers .= "Return-path: holahola@desarrolloweb.com\r\n"; 
+
+//direcciones que recibión copia 
+$headers .= "Cc: luis.claudio@overprimegroup.com\r\n";
+//$headers .= "Cc: martha.peralta@codrise.com\r\n";
+//$headers .= "Cc: elvis.janampa@codrise.com\r\n"; 
+
+//direcciones que recibirón copia oculta 
+//$headers .= "Bcc: pepe@pepe.com,juan@juan.com\r\n"; 
+
+mail($destinatario,$asunto,$cuerpo,$headers);
+
+header('Location: http://192.168.1.7/rockdrill/reserva/pages/reserva-kit?tipo='.$_GET[tipo].'&msj='.'ok');
 }
 
 else
