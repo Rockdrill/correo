bb01feb86e38efdcecbb632d4e641a08a3b99cbd
claudito <megabyte1507@gmail.com>
2016-03-02 14:33:53 -0500

    add ticket-trasnferido

---
+++ ticket/ticket-transferido.php
@@ -0,0 +1,73 @@
+<?php 
+if (isset($_GET['usuario']) AND isset($_GET['ticket'])) 
+{
+  
+$destinatario =$_GET['correousuario'];
+$remitente    =$_GET['correosoporte'];
+$asunto       = "Ticket Asignado N°".$_GET['ticket']; 
+$cuerpo =' 
+<html>
+<head>
+<title>Ticket Reasignado</title>
+<style>
+
+body{font-family: arial;   font-weight: bold;}
+
+</style>
+</head>
+<body>
+<h1>Ticket Reasignado # '.$_GET[ticket].'</h1>
+<hr>
+<h2>Hola '.$_REQUEST[usuario].' el ticket # '.$_GET[ticket].'
+fue reasignado a  '.$_GET[usuariosoporte].'.
+<br>
+Se contactara contigo para poder resolver el detalle de tu ticket generado.
+<hr>
+Detalle de ticket:
+<br>
+'.$_REQUEST[detalleticket].'
+<br>
+<hr>
+Atentamente.
+<br>
+@soporte_ti
+</body>
+</html>
+'; 
+
+$headers .= 'From: '.$remitente."\r\n".
+'Reply-To: '.$remitente."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+
+//para el envío en formato HTML 
+$headers .= "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=UTF-8\r\n"; 
+
+//dirección del remitente 
+//$headers .= "From: Luis Claudio <luis.claudio@overprimegroup.com>\r\n"; 
+
+//dirección de respuesta, si queremos que sea distinta que la del remitente 
+//$headers .= "Reply-To: mariano@desarrolloweb.com\r\n"; 
+
+//ruta del mensaje desde origen a destino 
+//$headers .= "Return-path: holahola@desarrolloweb.com\r\n"; 
+
+//direcciones que recibión copia 
+$headers .= "Cc: $_GET[correosoporte]\r\n";
+
+//direcciones que recibirón copia oculta 
+//$headers .= "Bcc: pepe@pepe.com,juan@juan.com\r\n"; 
+
+mail($destinatario,$asunto,$cuerpo,$headers);
+
+header('Location: http://192.168.1.8/sistemas/consulta/asignar?id=ok');
+}
+else
+{
+  header('Location: http://www.incomica.com/wp-content/uploads/2014/01/no-existe-el-infierno.jpg');
+}
+
+
+
+?>
+
