10c41b069d602bae2345d5e0996d8ceb428362b5
claudito <megabyte1507@gmail.com>
2016-08-23 10:52:44 -0500

    correo costos

---
+++ codrise/mail_notificacion_lista_costos.php
@@ -0,0 +1,64 @@
+<?php
+if (isset($_POST['remitente'])) 
+{
+  
+$destinatario = $_POST['destinatario'];
+$remitente    = $_POST['remitente'];
+$asunto       = "Lista de Confirmación de Evaluación"; 
+$cuerpo =' 
+<html> 
+<head> 
+<title>Lista de Confirmación de Evaluación</title>
+</head> 
+<body> 
+<h2>Lista de Confirmación de Evaluación</h2>
+<hr> 
+<p> 
+'.$_POST['contenido'].'
+</p> 
+</body> 
+</html> 
+'; 
+
+$headers .= 'From: '.$remitente."\r\n".
+'Reply-To: '.$remitente."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+
+//para el envío en formato HTML 
+$headers .= "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=UTF-8\r\n"; 
+
+//dirección del remitente 
+//$headers .= "From: Luis Claudio <luis.claudio@overprimegroup.com>\r\n"; 
+
+//dirección de respuesta, si queremos que sea distinta que la del remitente 
+//$headers .= "Reply-To: mariano@desarrolloweb.com\r\n"; 
+
+//ruta del mensaje desde origen a destino 
+//$headers .= "Return-path: holahola@desarrolloweb.com\r\n"; 
+
+//direcciones que recibión copia 
+//$headers .= "Cc: wilder.garcia@codrise.com\r\n";
+//$headers .= "Cc: martha.peralta@codrise.com\r\n";
+//$headers .= "Cc: luis.claudio@codrise.com\r\n"; 
+
+//direcciones que recibirón copia oculta 
+//$headers .= "Bcc: pepe@pepe.com,juan@juan.com\r\n"; 
+
+mail($destinatario,$asunto,$cuerpo,$headers);
+
+header('Location: http://192.168.1.7/reserva/pages/detalle-lista-evaluacion?id='.$_POST['id'].'&m='.'ok');
+}
+
+else
+{
+  header('Location: http://www.incomica.com/wp-content/uploads/2014/01/no-existe-el-infierno.jpg');
+
+}
+
+
+
+
+
+
+?>
